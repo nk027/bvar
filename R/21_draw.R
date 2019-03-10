74800bf91b5913f82754b9b443a6fcbde9c4674e
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2019-03-10 16:22:45 +0100

    Wrap up ml and draw for now

--- R/21_draw.R
+++ R/21_draw.R
@@ -1,4 +1,6 @@
-bv_draw <- function(){
+bv_draw <- function(psi, sse, beta_hat, omega_inv,
+                    b, N, M, lags) {
+
   S_post <- psi + sse +
     t(beta_hat - priors$b) %*% omega_inv %*% (beta_hat - priors$b)
   S_eig <- eigen(S_post)
@@ -7,9 +9,11 @@ bv_draw <- function(){
   sigma_draw <- solve(crossprod(eta)) %*% diag(M)
   sigma_chol <- t(chol(sigma_draw))
   beta_draw <- beta_hat +
-    t(MASS::mvrnorm(n = M,
-                    mu = rep(0, (1 + M * lags)),
-                    Sigma = solve(crossprod(X) + omega_inv) %*%
-                      diag(1 + M * lags))) %*%
+    t(MASS::mvrnorm(n = M, mu = rep(0, (1 + M * lags)),
+                    Sigma = solve(crossprod(X) + omega_inv) %*% diag(1 + M * lags))) %*%
     sigma_chol
+
+  out <- list("beta_draw" = beta_draw,
+              "sigma_draw" = sigma_draw,
+              "sigma_chol" = sigma_chol)
 }
