64565d1f3a4f9fd85c94aa83523b885f22924ce0
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2019-03-10 16:23:17 +0100

    Add short script to help test

---
+++ setup.R
@@ -0,0 +1,15 @@
+to_source <- c(11, 12, 40)
+
+sapply(list.files("R"), function(x) {
+  if(any(stringr::str_detect(x, as.character(to_source))))
+    source(paste0("R/", x))
+})
+
+data <- matrix(rnorm(200), ncol = 2)
+lags <- 2
+draws <- 10000
+burns <- 10000
+thin <- 1
+verbose <- TRUE
+priors <- bv_priors()
+priors <- bv_priors(soc = bv_dummy(fun = mean))
