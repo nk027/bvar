d060e453f4a5a715d57e27a0c2b004769c3da1fe
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2019-04-03 21:10:26 +0200

    Kick out current conditional fcast

--- R/50_fcast.R
+++ R/50_fcast.R
@@ -1,11 +1,11 @@
 bv_fcast <- function(
   horizon = 12,
-  exogenous = NULL) {
+  conditional = NULL) {
 
   horizon = int_check(horizon, min = 1, max = 1e6,
                       msg = "Invalid value for horizon (outside of [1, 1e6]).")
 
-  out <- list("horizon" = horizon, "exogenous" = exogenous)
+  out <- list("horizon" = horizon, "conditional" = conditional)
   class(out) <- "bv_fcast"
 
   return(out)
@@ -17,7 +17,7 @@ compute_fcast <- function(
   horizon,
   beta_comp, beta_const,
   sigma,
-  exogenous = NULL) {
+  conditional = NULL) {
 
   Y_f <- matrix(NA, horizon + 1, K - 1)
   Y_f[1, ] <- sapply(t(Y[N:(N - lags + 1), ]), c)
@@ -27,29 +27,6 @@ compute_fcast <- function(
       c(beta_const, rep(0, M * (lags - 1))) +
       c(sigma %*% rnorm(M), rep(0, M * (lags - 1)))
   }
-
-  # Removed exogenous forecasts for now
-  # # if(is.null(exogenous)) {
-  #   # Unconditional forecast
-  #   for(i in 2:(1 + horizon)) {
-  #     Y_f[i, ] <- Y_f[i - 1, ] %*% t(beta) +
-  #       c(const, rep(0, M + (lags - 1))) +
-  #       c(sigma %*% rnorm(M), rep(0, M * (lags - 1)))
-  #   }
-  # } else {
-  #   # Conditional forecast
-  #   Y_f[2:(exogenous$hor + 1), exogenous$pos] <- exogenous$path
-  #   for(i in 2:(1 + horizon)) { # same as unconditional
-  #     Y_temp <- Y_f[i - 1, ] %*% t(beta) +
-  #       c(const, rep(0, M * (lags - 1))) +
-  #       c(sigma %*% rnorm(M), rep(0, M * (lags - 1)))
-  #     if(i < (exogenous$hor + 2)) { # do no replace the exogenous path
-  #       Y_f[i, -exogenous$pos] <- Y_temp[-exogenous$pos]
-  #     } else {
-  #       Y_f[i, ] <- Y_temp
-  #     }
-  #   }
-  # }
 
   # Remove Y_t and further lags from Y_f to get the forecast
   out <- Y_f[2:(1 + horizon), 1:M]
