da32ce5abe635bf77996e3cbadc66ae9b1561b29
oDNAudio <lukas.vashold@gmx.at>
2019-04-12 19:28:09 +0200

    finally added printing method for bv_irf

--- R/60_irf.R
+++ R/60_irf.R
@@ -21,3 +21,47 @@ bv_irf <- function(
 
   return(out)
 }
+
+
+print.bv_irf <- function(x, ...) {
+
+  cat("\nProvide object to function bvar() in order to compute impulse responses",
+      "\nfor a horizon of", x$horizon, "time periods.")
+  if(x$identification){
+    if(is.null(x$sign_restr)){
+      cat("\n\nIdentification is achieved by means of Cholesky decomposition.",
+          "\nConsider ordering of variables for correct transmission of",
+          "\ncontemporaneous effects between variables.")
+    }else{
+
+      signs_df <- as.data.frame(x$sign_restr)
+      signs_df <- apply(signs_df, 2, factor,
+                        levels = c(-1, 0, 1), labels = c("+", "0", "-"))
+
+      cat("\n\nIdentification is achieved by means of signs restrictions.")
+      cat("\nChosen restrictions:")
+
+      paste_it <- paste0("Var", 1:nrow(signs_df), "\t")
+      for(i in 1:ncol(signs_df)){
+        temp_paste <- paste0(" ", signs_df[ , i], sep="\t")
+        paste_it <- paste0(paste_it, temp_paste)
+      }
+      paste_it <- paste0(c("\nResponse of\t",
+                           rep("\n\t\t", (nrow(signs_df) - 1))),
+                         paste_it)
+      cat("\n\t\t\tShock from",
+          "\n\t\t\t",  paste0("Var", c(1:nrow(signs_df)), sep = "\t"),
+          paste_it, sep = "")
+
+      cat("\n\nColums contain the expected sign of the reaction of all",
+          "\nvariables following a shock of the respective variable.")
+    }
+  }else{
+    cat("\n\nShocks are unidentified. Note that interpretation of impulse",
+        "\nresponses may be flawed.")
+  }
+  if(x$fevd){
+    cat("\n\nForecast error variance decompositions will be computed.")
+  }
+
+}
