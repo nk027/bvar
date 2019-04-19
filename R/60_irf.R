892b19b0746c8134c85669b99270180c5dd2bab0
oDNAudio <lukas.vashold@gmx.at>
2019-04-19 19:03:39 +0200

    added doku to IRF stuff. work in progress.

--- R/60_irf.R
+++ R/60_irf.R
@@ -1,3 +1,25 @@
+#' Impulse response constructor
+#'
+#' Constructs object to be passed on to `bvar()` for computation of impulse
+#' responses
+#'
+#' @param horizon Integer scalar. Specifies the horizon for which impulse
+#' responses should be computed.
+#' @param fevd Logical scalar. Specifies whether or not forecast error variance
+#' decompositions should be calculated.
+#' @param identification Logical scalar. Specifies whether or not the shocks
+#' used for calculating the impulse should be identified. Default is TRUE,
+#' identification will then be achieved recursively i.e. through a Cholesky
+#' decomposition of the vcov-matrix if \emph{sign_restr} is NULL. If set to
+#' FALSE, shocks will be unidentified.
+#' @param sign_restr Numeric matrix. Specifies sign restrictions for
+#' identification. Elements should be set to \eqn{1} (\eqn{-1}) to restrict for
+#' a positive (negative) impact on the respective variables. If no presumption
+#' about the impact is available, set corresponding elements to \eqn{0}.
+#'
+#' @return Returns a list of parameters as an object of class "bv_irf" that can
+#' be passed on to function `bvar()`.
+
 bv_irf <- function(
   horizon = 12,
   fevd = FALSE,
--- R/61_irf_compute.R
+++ R/61_irf_compute.R
@@ -1,3 +1,33 @@
+#' Impulse response draws
+#'
+#' Computes impulse responses using the posterior draws of the VAR coefficients
+#' and vcov-matrix obtained from `bv_draw()`.
+#'
+#' @param beta_comp Numeric matrix. Posterior draw of the VAR coefficients of
+#' the model in state space representation.
+#' @param sigma_draw Numeric matrix. Posterior draw of the vcoc-matrix of the
+#' model.
+#' @param sigma_chol Numeric matrix. Lower part of the Choleski decomposition
+#' of sigma_draw. Calculated as \code{t(chol(sigma_draw))}.
+#' @param M Integer scalar. Columns of \emph{X}.
+#' @param lags Integer scalar. Number of lags in the model.
+#' @param horizon Integer scalar. Specifies the horizon for which impulse
+#' responses should be computed.
+#' @param identification Logical scalar. Specifies whether or not the shocks
+#' used for calculating the impulse should be identified. Default is TRUE,
+#' identification will then be achieved recursively i.e. through a Cholesky
+#' decomposition of the vcov-matrix if \emph{sign_restr} is NULL. If set to
+#' FALSE, shocks will be unidentified.
+#' @param sign_restr Numeric matrix. Elements inform about expected impacts
+#' of certain shocks. Can be either \eqn{1}, \eqn{-1} or \eqn{0}, depending
+#' on whether a positive, a negative or no contemporaneous effect of a certain
+#' shock is expected.
+#' @param fevd Logical scalar. Specifies whether or not forecast error variance
+#' decompositions should be calculated.
+#'
+#' @return Returns a matrix used as shock for computations of impulse responses
+#' identified via sign restrictions.
+
 compute_irf <- function(
   beta_comp,
   sigma, sigma_chol,
