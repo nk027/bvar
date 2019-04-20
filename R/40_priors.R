978a42ce2a11968308a81785178f77e179a5f52a
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2019-04-20 20:39:45 +0200

    Add docs to 40_prior

--- R/40_priors.R
+++ R/40_priors.R
@@ -1,3 +1,31 @@
+#' Prior settings
+#'
+#' Helper function to set up parameters and informations on priors for
+#' \code{\link{bvar}}. Used for adjusting the parameters treated as
+#' hyperparameters, the Minnesota prior and adding various dummy priors.
+#'
+#' @param hyper Character vector. Used to specify the parameters to be treated
+#' as hyperparameters. Can also be set to "auto" or "full" for
+#' an automatic / full subset. Other allowed values are the Minnesota prior's
+#' parameters "lambda", "alpha" and "psi" as well as the names of possible
+#' dummy priors.
+#' @param mn List of class "bv_minnesota". Options for the Minnesota prior,
+#' set via \code{\link{bv_minnesota}}.
+#' @param ... Optional lists of class "bv_dummy" with options for possible
+#' dummy priors. Must be assigned a name in the function call. Created with
+#' \code{\link{bv_dummy}}
+#'
+#' @return Returns a named list with options for \code{\link{bvar}}.
+#' @export
+#'
+#' @seealso \code{\link{bv_minnesota}}, \code{\link{bv_dummy}}
+#'
+#' @examples
+#' # Extending hyperparameters
+#' bv_priors(c("lambda", "alpha", "psi"))
+#'
+#' # Adding already constructed dummy priors
+#' bv_priors(hyper = "auto", "soc" = soc_prior, "sur" = sur_prior)
 bv_priors <- function(
   hyper = "auto",
   mn = bv_minnesota(bv_lambda(0.2, 0.4, 0.0001, 5),
@@ -8,11 +36,11 @@ bv_priors <- function(
   # Check inputs ------------------------------------------------------------
 
   if(!is.null(mn) && !inherits(mn, "bv_minnesota")) {
-    stop("Please use 'bv_minnesota()' to set the minnesota prior.")
+    stop("Please use `bv_minnesota()` to set the minnesota prior.")
   }
   dots <- list(...)
   if(!all(vapply(dots, inherits, TRUE, "bv_dummy"))) {
-    stop("Please use 'bv_dummy()' to set dummy priors.")
+    stop("Please use `bv_dummy()` to set dummy priors.")
   }
   if(hyper[[1]] == "auto") {
     hyper <- c(if(!is.null(mn)) "lambda", names(dots))
