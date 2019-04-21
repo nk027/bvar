469b46bd17bfa8880920fcc3195a7ba7bdc3cb96
oDNAudio <lukas.vashold@gmx.at>
2019-04-21 16:30:20 +0200

    included dataset (hopefully right)

---
+++ R/data_fred_qd.R
@@ -0,0 +1,30 @@
+#' FRED-QD: Quarterly Database for Macroeconomic Research
+#'
+#' FRED-QD is a large macroeconomic database designed for the empirical analysis
+#' of “big data”. They are updated in real-time through the FRED database. They
+#' are publicly accessible, facilitating the replication of empirical work. And
+#' they relieve the researcher of the task of incorporating data changes and
+#' revisions (a task accomplished by the data desk at the Federal Reserve Bank
+#' of St. Louis). The current version included is from April 2019 and contains
+#' quarterly observations for many macroeconomic variables from Q1 1959 to Q4
+#' 2018.
+#'
+#' @docType data
+#'
+#' @usage data(fred_qd)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @references McCracken & Ng (2016) Journal of Business & Economic Statistics
+#' 34.4:574-589
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#'
+#'
+#' @source \href{https://research.stlouisfed.org/econ/mccracken/fred-databases/}{McCracken website}
+#'
+#' @examples
+#' data(fred_qd)
+
