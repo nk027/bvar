72526753809960f2908996ee60ed6700476132f9
oDNAudio <lukas.vashold@gmx.at>
2019-04-27 14:51:31 +0200

    added small examples with dummies, working now

--- setup.R
+++ setup.R
@@ -53,11 +53,44 @@ data_small_VAR <- fred_qd[, c("CPIAUCSL", "UNRATE", "FEDFUNDS")]
 data_small_VAR[5:nrow(data_small_VAR), 1] <- diff(log(data_small_VAR[, 1]),
                                                   lag = 4) * 100
 data_small_VAR <- data_small_VAR[5:nrow(data_small_VAR),]
-run7 <- bvar(data_small_VAR, lags = 5, irf = irf, verbose = TRUE)
+
+
+
+add_soc <- function(Y, lags, par) {
+  soc <- if(lags == 1) {diag(Y[1, ]) / par} else {
+    diag(colMeans(Y[1:lags, ])) / par
+  }
+  Y_soc <- soc
+  X_soc <- cbind(rep(0, ncol(Y)), matrix(rep(soc, lags), nrow = ncol(Y)))
+
+  return(list("Y" = Y_soc, "X" = X_soc))
+}
+
+soc <- bv_dummy(mode = 1, sd = 1, min = 0.0001, max = 50, fun = add_soc)
+
+
+add_sur <- function(Y, lags, par) {
+  sur <- if(lags == 1) {Y[1, ] / par} else {
+    colMeans(Y[1:lags, ]) / par
+  }
+  Y_sur <- sur
+  X_sur <- c(1 / par, rep(sur, lags))
+
+  return(list("Y" = Y_sur, "X" = X_sur))
+}
+
+sur <- bv_dummy(mode = 1, sd = 1, min = 0.0001, max = 50, fun = add_sur)
+
+
+priors_dum <- bv_priors(hyper = "full", "soc" = soc, "sur" = sur, mn = bv_mn(alpha = bv_alpha(mode = 25, max = 3)))
+
+
+run7 <- bvar(data_small_VAR, priors = priors_dum, lags = 5, irf = irf, verbose = TRUE)
 
 signs <- matrix(c(1,1,-1,-1,1,-1,-1,1,1), nrow = 3)
 irf_signs <- bv_irf(sign_restr = signs)
-run8 <- bvar(data_small_VAR, lags = 5, irf = irf_signs, verbose = TRUE)
+
+run8 <- bvar(data_small_VAR, priors = priors_dum, lags = 5, irf = irf_signs, verbose = TRUE)
 
 bv_plot_irf(run7)
 bv_plot_irf(run8)
