da98ac716a47f8d49158c8bf58d7a182b3f7d40a
oDNAudio <lukas.vashold@gmx.at>
2019-05-03 20:43:51 +0200

    fixed acceptance rate computation

--- R/90_method.R
+++ R/90_method.R
@@ -32,7 +32,8 @@ print.bvar <- function(x, ...) {
   cat("\nIterations (burnt / thinning): ", x$meta$n_draw, " (", x$meta$n_burn,
       "/", x$meta$n_thin, ")", sep = "")
   cat("\nAccepted draws (rate): ", x$meta$accepted, " (",
-      round(x$meta$accepted / x$meta$n_save, 3), ")\n", sep = "")
+      round(x$meta$accepted / (x$meta$n_draw - x$meta$n_burn), 3),
+      ")\n", sep = "")
 
   if(!is.null(x$irf)) {cat("\n"); print(x$irf)}
   if(!is.null(x$fcast)) {cat("\n"); print(x$fcast)}
