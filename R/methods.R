dec1c8622fd6463beba1e9d9ad0b2debf62e0ad6
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2019-08-26 01:39:04 +0200

    Add some calcium

---
+++ R/methods.R
@@ -0,0 +1,46 @@
+predict.bvar <- function(x, settings, thin = 1L) {
+
+  get_beta_const(beta_draw)
+  get_beta_comp(beta_draw, K, M, lags)
+  get_sigma_chol(sigma_draw)
+
+}
+
+irf.bvar <- function(x, settings, thin = 1L) {
+
+  get_beta_comp(beta_draw, K, M, lags)
+  get_sigma_chol(sigma_draw)
+
+}
+
+fevd.bvar() <- function(x) {}
+
+summary.bvar() <- function(x) {
+
+  print()
+  add_fancy_info()
+
+}
+
+fitted.bvar() <- function(x) {
+
+
+
+}
+
+residuals.bvar <- function(x) {
+
+  Y - fitted.bvar(x)
+
+}
+
+coef.bvar <- function(x) {}
+
+vcov.bvar <- function(x) {}
+
+logLik.bvar <- function(x) {}
+
+Phi.bvar <- function(x) {}
+
+Psi.bvar <- function(x) {}
+
