074cf6ec5e8036102ba3d416a5853f094189cbea
oDNAudio <lukas.vashold@gmx.at>
2019-09-02 10:56:08 +0200

    added doc to coda conversion tool

--- R/70_coda_checks.R
+++ R/70_coda_checks.R
@@ -1,4 +1,41 @@
-#' @noRd
+#' Method for converting hyperparameter chains to coda-compatible objects
+#'
+#' Method to convert chains of hyperparameters and marginal likelihood obtained
+#' from \code{\link{bvar}} to objects compatible for further processing by
+#' \code{\link{coda}} i.e., objects of class \code{\link[coda]{mcmc}} or
+#' \code{\link[coda]{mcmc.list}}.
+#' Resulting objects may be subset of all hyperparameters using \emph{vars}.
+#' Multiple chains, i.e. comparable \code{bvar} objects may be converted using
+#' the \emph{chains} argument.
+#'
+#' @param x A \code{bvar} object, obtained from \code{\link{bvar}}.
+#' @param vars Optional character vector used to subset the the converted
+#' hyperparameters. The elements need to match the names of hyperparameters
+#' (including \code{"ml"}). Defaults to \code{NULL}, i.e. all variables.
+#' @param chains List with additional \code{bvar} objects. Contents are then
+#' converted to an object of class \code{\link[coda]{mcmc.list}}.
+#' @param ... Other parameters for \code{\link[coda]{as.mcmc}} and
+#' \code{\link[coda]{as.mcmc.list}}.
+#'
+#' @seealso
+#'
+#' @keywords VAR BVAR coda mcmc object convergence
+#'
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' data <- matrix(rnorm(200), ncol = 2)
+#' x <- bvar(data, lags = 2)
+#' y <- bvar(data, lags = 2)
+#' z <- bvar(data, lags = 2)
+#'
+#' # Plot full traces and densities
+#' as.mcmc(x, vars = c("ml", "lambda"))
+#'
+#' # Add second chain for further processing
+#' as.mcmc(x, vars = c("ml", "lambda"), chains = list(y = y, z = z))
+#' }
 as.mcmc.bvar <- function(x, vars = NULL, chains = list(), ...) {
 
   if(!inherits(x, "bvar")) {stop("Please provide a `bvar` object.")}
