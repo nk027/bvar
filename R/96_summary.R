a771eb49ec856aeaf48d0c38cbc5d7a000ad3fb0
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2019-09-03 15:28:57 +0200

    Edit summary to calculate before printing

--- R/96_summary.R
+++ R/96_summary.R
@@ -4,10 +4,14 @@ summary.bvar <- function(x, ...) {
 
   if(!inherits(x, "bvar")) {stop("Please provide a `bvar` object.")}
 
+  coef_x <- coef(x)
+  vcov_x <- vcov(x)
+  logLik_x <- logLik(x)
+
   print(x)
-  cat("\n"); print(coef(x))
-  cat("\n"); print(vcov(x))
-  cat("\n"); print(logLik(x))
+  cat("\n"); print(coef_x)
+  cat("\n"); print(vcov_x)
+  cat("\n"); cat("Log-Likelihood: ", logLik_x)
 
   return(invisible(x))
 }
