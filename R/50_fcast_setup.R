1241506392ce82faff73e6b76378c7548efcd41d
oDNAudio <41572883+oDNAudio@users.noreply.github.com>
2020-02-18 18:35:49 +0100

    changes to fcast setup
    conditional setting is not needed anymore, as I would suggest to allow the user to calculate conditional forecasts only ex-post.

--- R/50_fcast_setup.R
+++ R/50_fcast_setup.R
@@ -3,13 +3,12 @@
 #' Provide forecast settings to \code{\link{bvar}} or
 #' \code{\link{predict.bvar}}. Allows adjusting the horizon of the forecast.
 #'
-#' As of now only unconditional forecasts are implemented and conditional must
-#' be set to \code{FALSE}. Conditional forecasts will require further options,
-#' such as variable paths.
+#' Conditional forecasts must be retrieved via \code{\link{scenario}}
+#' ex-post. Conditional forecasts will require further options, such as
+#' variable paths.
 #'
 #' @param horizon Integer scalar. Horizon for which forecasts should be
 #' computed.
-#' @param conditional Logical scalar. Not yet implemented.
 #'
 #' @return Returns a named list of class \code{bv_fcast} with options for
 #' \code{\link{bvar}} or \code{\link{predict.bvar}}.
@@ -24,13 +23,12 @@
 #' # Set the forecast-horizon to 20 time periods instead of 12
 #' bv_fcast(horizon = 20)
 bv_fcast <- function(
-  horizon = 12,
-  conditional = FALSE) {
+  horizon = 12) {
 
   horizon <- int_check(horizon, min = 1, max = 1e6,
                        msg = "Invalid value for horizon (outside of [1, 1e6]).")
 
-  if(conditional) {stop("Conditional forecasts not yet implemented.")}
+  # if(conditional) {stop("Conditional forecasts not yet implemented.")}
 
   out <- list("horizon" = horizon, "conditional" = conditional)
   class(out) <- "bv_fcast"
