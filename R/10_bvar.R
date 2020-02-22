4ac51fd5948a3b880b5885e2c154ff965a98fd2e
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2020-02-22 15:49:28 +0100

    Remove irf and predict from loop, rm n_save

--- R/10_bvar.R
+++ R/10_bvar.R
@@ -14,9 +14,8 @@
 #' @param data Numeric matrix or dataframe. Note that observations need to be
 #' ordered from earliest to latest one.
 #' @param lags Integer scalar. Number of lags to apply to the data.
-#' @param n_draw,n_burn,n_save Integer scalar. Define the number of iterations
-#' to (a) cycle through, (b) burn at the start, (c) save. Note that if provided
-#' \emph{n_save} takes precedence over \emph{n_draw}.
+#' @param n_draw,n_burn Integer scalar. Define the number of iterations
+#' to (a) cycle through and (b) burn at the start.
 #' @param n_thin Integer scalar. Provides the option of reducing the number of
 #' stored iterations to every \emph{n_thin}'th one. The number of saved
 #' iterations thus equals \emph{(n_draw - n_burn) / n_thin}.
@@ -103,7 +102,7 @@
 #' }
 bvar <- function(
   data, lags,
-  n_draw = 10000L, n_burn = 5000L, n_save, n_thin = 1L,
+  n_draw = 5000L, n_burn = 1000L, n_thin = 1L,
   priors = bv_priors(),
   mh = bv_mh(),
   fcast = NULL,
@@ -126,17 +125,12 @@ bvar <- function(
 
   # Integers
   lags <- int_check(lags, min = 1L, max = nrow(Y))
-  n_draw <- int_check(n_draw, min = 1L)
+  n_draw <- int_check(n_draw, min = 1L, msg = "Issue with n_draw.")
   n_burn <- int_check(n_burn, min = 0L, max = n_draw - 1L,
     msg = "Issue with n_burn. Is n_burn < n_draw?")
   n_thin <- int_check(n_thin, min = 1L, max = ((n_draw - n_burn) / 10),
     msg = "Issue with n_thin. Maximum allowed is (n_draw - n_burn) / 10.")
-  if(missing(n_save)) {
-    n_save <- int_check(((n_draw - n_burn) / n_thin), min = 1L)
-  } else {
-    n_save <- int_check(n_save, min = 1L, msg = "Issue with n_save.")
-    n_draw <- int_check(n_save + n_burn, min = 1L)
-  }
+  n_save <- int_check(((n_draw - n_burn) / n_thin), min = 1L)
 
   # Constructors, required
   if(!inherits(priors, "bv_priors")) {
@@ -158,7 +152,7 @@ bvar <- function(
 
   # Preparation -------------------------------------------------------------
 
-  X <- lag_var(Y, lags)
+  X <- lag_var(Y, lags = lags)
 
   Y <- Y[(lags + 1):nrow(Y), ]
   X <- X[(lags + 1):nrow(X), ]
@@ -172,22 +166,11 @@ bvar <- function(
   variables <- name_deps(variables = colnames(data), M = M)
   explanatories <- name_expl(variables = variables, M = M, lags = lags)
 
-  # # Check sign restrictions
-  # if(!is.null(irf[["sign_restr"]]) && length(irf[["sign_restr"]]) != M ^ 2) {
-  #   stop("Dimensions of provided sign restrictions do not fit the data.")
-  # }
 
-  # # Check and get conditioning matrix for forecasts
-  # if(!is.null(fcast) && !is.null(fcast[["conditional"]])) {
-  #   cond_mat <- get_cond_mat(path = fcast[["conditional"]][["path"]],
-  #                            horizon= fcast[["horizon"]],
-  #                            cond_var = fcast[["conditional"]][["cond_var"]],
-  #                            variables = variables, M = M)
-  # }
+  # Priors -----
 
-  # Priors ------------------------------------------------------------------
+  # Minnesota prior ---
 
-  # Minnesota prior
   if(length(priors[["b"]]) == 1 && priors[["b"]] == "auto") {
     priors[["b"]] <- matrix(0, nrow = K, ncol = M)
     priors[["b"]][2:(M + 1), ] <- diag(M)
@@ -205,15 +188,15 @@ bvar <- function(
     stop("Dimensions of psi do not fit the data.")
   }
 
-  # Parameters
+  # Parameters ---
   pars_names <- names(priors)[!names(priors) %in% c("hyper", "var", "b")]
   pars_full <- do.call(c, lapply(pars_names, function(x) priors[[x]][["mode"]]))
   names(pars_full) <- name_pars(pars_names, M)
 
-  # Dummy priors
+  # Dummy priors ---
   priors[["dummy"]] <- pars_names[!pars_names %in% c("lambda", "alpha", "psi")]
 
-  # Hierarchical priors
+  # Hierarchical priors ---
   hyper_n <- length(priors[["hyper"]]) +
     sum(priors[["hyper"]] == "psi") * (M - 1)
   if(hyper_n == 0) {stop("Please provide at least one hyperparameter.")}
@@ -224,13 +207,13 @@ bvar <- function(
   hyper_max <- do.call(c, lapply(priors[["hyper"]], get_priors, par = "max"))
   names(hyper) <- name_pars(priors[["hyper"]], M)
 
-  # Split up psi
+  # Split up psi ---
   for(i in seq_along(priors[["psi"]][["mode"]])) {
     priors[[paste0("psi", i)]] <- lapply(priors[["psi"]], function(x) x[i])
   }
 
 
-  # Optimise ----------------------------------------------------------------
+  # Optimise and draw -----
 
   opt <- optim(par = hyper, bv_ml, gr = NULL,
     hyper_min = hyper_min, hyper_max = hyper_max, pars = pars_full,
@@ -248,7 +231,7 @@ bvar <- function(
   }
 
 
-  # Hessian -----------------------------------------------------------------
+  # Hessian ---
 
   H <- diag(length(opt[["par"]])) * mh[["scale_hess"]]
   J <- unlist(lapply(names(hyper), function(name) {
@@ -260,13 +243,13 @@ bvar <- function(
 
   # Make sure HH is positive definite
   if(hyper_n != 1) {
-    HH_eig <- eigen(HH)
+    HH_eig <- eigen(HH, symmetric = TRUE)
     HH <- HH_eig[["vectors"]] %*% diag(abs(HH_eig[["values"]])) %*%
       t(HH_eig[["vectors"]])
   } else {HH <- abs(HH)}
 
 
-  # Initial draw ------------------------------------------------------------
+  # Initial draw ---
 
   while(TRUE) {
     hyper_draw <- rmvnorm(n = 1, mean = opt[["par"]], sigma = HH)[1, ]
@@ -277,9 +260,9 @@ bvar <- function(
   }
 
 
-  # Loop --------------------------------------------------------------------
+  # Sampling -----
 
-  # Storage
+  # Storage ---
   accepted <- 0 -> accepted_adj # Beauty
   ml_store <- vector("numeric", n_save)
   hyper_store <- matrix(NA, nrow = n_save, ncol = length(hyper_draw),
@@ -287,28 +270,10 @@ bvar <- function(
   beta_store <- array(NA, c(n_save, K, M))
   sigma_store <- array(NA, c(n_save, M, M))
 
-  # if(!is.null(irf)) {
-  #   irf_store <- list(
-  #     "irf" = array(NA, c(n_save, M, irf[["horizon"]], M)),
-  #     "fevd" = if(irf[["fevd"]]) {
-  #       structure(
-  #         list("fevd" = array(NA, c(n_save, M, irf[["horizon"]], M)),
-  #           "variables" = variables), class = "bvar_fevd")
-  #     } else {NULL},
-  #     "setup" = irf, "variables" = variables)
-  #   class(irf_store) <- "bvar_irf"
-  # }
-  # if(!is.null(fcast)) {
-  #   fcast_store <- list(
-  #     "fcast" = array(NA, c(n_save, fcast[["horizon"]], M)),
-  #     "setup" = fcast, "variables" = variables, "data" = Y)
-  #   class(fcast_store) <- "bvar_fcast"
-  # }
-
-  # Loop
   if(verbose) {pb <- txtProgressBar(min = 0, max = n_draw, style = 3)}
 
-  for(i in (1 - n_burn):(n_draw - n_burn)) { # Start loop
+  # Start loop ---
+  for(i in (1 - n_burn):(n_draw - n_burn)) {
 
     # Metropolis-Hastings
     hyper_temp <- rmvnorm(n = 1, mean = opt[["par"]], sigma = HH)[1, ]
@@ -348,53 +313,6 @@ bvar <- function(
       beta_store[(i / n_thin), , ] <- draws[["beta_draw"]]
       sigma_store[(i / n_thin), , ] <- draws[["sigma_draw"]]
 
-      # # Companion matrix is used for both forecasts and impulse responses
-      # if(!is.null(fcast) || !is.null(irf)) {
-      #   beta_comp <- get_beta_comp(beta = draws[["beta_draw"]],
-      #     K = K, M = M, lags = lags)
-      # }
-
-      # if(!is.null(irf)) { # Impulse responses
-      #   irf_comp <- compute_irf(beta_comp = beta_comp,
-      #     sigma = draws[["sigma_draw"]], sigma_chol = draws[["sigma_chol"]],
-      #     M = M, lags = lags, horizon = irf[["horizon"]],
-      #     identification = irf[["identification"]],
-      #     sign_restr = irf[["sign_restr"]], sign_lim = irf[["sign_lim"]],
-      #     fevd = irf[["fevd"]])
-      #   irf_store[["irf"]][(i / n_thin), , , ] <- irf_comp[["irf"]]
-      #   if(irf[["fevd"]]) { # Forecast error variance decomposition
-      #     irf_store[["fevd"]][(i / n_thin), , , ] <- irf_comp[["fevd"]]
-      #   }
-      # } # End impulse responses
-
-      # if(!is.null(fcast)) { # Forecast
-      #   if(is.null(fcast[["conditional"]])) { # unconditional
-      #     fcast_store[["fcast"]][(i / n_thin), , ] <- compute_fcast(Y = Y,
-      #        K = K, M = M, N = N, lags = lags, horizon = fcast[["horizon"]],
-      #        beta_comp = beta_comp, beta_const = draws[["beta_draw"]][1, ],
-      #        sigma = draws[["sigma_draw"]],
-      #        conditional = FALSE)
-      #   } else { # conditional
-      #     if(is.null(irf) ||
-      #        irf[["horizon"]] < fcast[["horizon"]] ||
-      #        !irf[["identification"]]) {
-      #       irf_comp <- compute_irf(beta_comp = beta_comp,
-      #         sigma = draws[["sigma_draw"]], sigma_chol = draws[["sigma_chol"]],
-      #         M = M, lags = lags, horizon = fcast[["horizon"]],
-      #         identification = TRUE,
-      #         sign_restr = NULL, sign_lim = NULL, fevd = FALSE)
-      #     }
-      #     noshock_fcast <- compute_fcast(Y = Y,
-      #       K = K, M = M, N = N, lags = lags, horizon = fcast[["horizon"]],
-      #       beta_comp = beta_comp, beta_const = draws[["beta_draw"]][1, ],
-      #       sigma = draws[["sigma_draw"]],
-      #       conditional = TRUE)
-      #     fcast_store[["fcast"]][(i / n_thin), , ] <- get_cond_fcast(M = M,
-      #       cond_mat = cond_mat, noshock_fcast = noshock_fcast,
-      #       ortho_irf = irf_comp[["irf"]], horizon = fcast[["horizon"]])
-      #   }
-      # } # End forecast
-
     } # End store
 
     if(verbose) {setTxtProgressBar(pb, (i + n_burn))}
@@ -409,9 +327,9 @@ bvar <- function(
   }
 
 
-  # Outputs -----------------------------------------------------------------
+  # Outputs -----
 
-  out <- list(
+  out <- structure(list(
     "beta" = beta_store, "sigma" = sigma_store,
     "hyper" = hyper_store, "ml" = ml_store,
     "optim" = opt, "priors" = priors,
@@ -421,29 +339,18 @@ bvar <- function(
       "explanatories" = explanatories,
       "n_draw" = n_draw, "n_burn" = n_burn, "n_save" = n_save,
       "n_thin" = n_thin
-    )
-  )
+    )), class = "bvar")
 
   if(!is.null(irf)) {
     out[["irf"]] <- tryCatch(irf.bvar(out, irf), function(e) {
       warning("Impulse response calculation failed with:\n", e)
-      return(NULL)
-    })
-    # irf_store[["quants"]] <- apply( # Add confidence bands
-    #   irf_store[["irf"]], c(2, 3, 4), quantile, c(0.16, 0.50, 0.84))
-    # out[["irf"]] <- irf_store
+      return(NULL)})
   }
   if(!is.null(fcast)) {
     out[["fcast"]] <- tryCatch(predict.bvar(out, fcast), function(e) {
-      warning("Impulse response calculation failed with:\n", e)
-      return(NULL)
-    })
-    # fcast_store[["quants"]] <- apply( # Add confidence bands
-    #   fcast_store[["fcast"]], c(2, 3), quantile, c(0.16, 0.50, 0.84))
-    # out[["fcast"]] <- fcast_store
+      warning("Forecast calculation failed with:\n", e)
+      return(NULL)})
   }
-
-  class(out) <- "bvar"
 
   return(out)
 }
