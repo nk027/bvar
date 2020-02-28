f2799f5a66680526f191967eb261b9f3c77a0a87
oDNAudio <lukas.vashold@gmx.at>
2020-02-28 13:12:58 +0100

    fixed small typo

--- R/93_density.R
+++ R/93_density.R
@@ -18,7 +18,7 @@
 #'
 #' @param mar Numeric vector. Margins for \code{\link[graphics]{par}}.
 #' @param mfrow Numeric vector. Rows for \code{\link[graphics]{par}}.
-#' @param var,n_vars,lag Integer scalars. Retrieve the position of lag
+#' @param var,n_vars,lag Integer scalars. Retrieve the position of
 #' \emph{lag} of variable \emph{var} given \emph{n_vars} total variables.
 #'
 #' @return Returns a list with outputs of \code{\link[stats]{density}}.
