4481b7b226b1ccdf2918c244b9d5073e55c58dc4
Nikolas Kuschnig <nkuschni@wu.ac.at>
2023-07-01 23:01:23 +0200

    Draft WAIC for model selection

--- R/94_logLik.R
+++ R/94_logLik.R
@@ -31,13 +31,13 @@
 #' # Calculate the log-likelihood
 #' logLik(x)
 #' }
-logLik.bvar <- function(object, ...) { # Todo: Maybe add conf_bands
+logLik.bvar <- function(object, type = c("mean", "quantile"), ...) {
 
   Y <- object[["meta"]][["Y"]]
   N <- object[["meta"]][["N"]]
   K <- object[["meta"]][["K"]]
-  mean <- fitted(object, conf_bands = 0.5)
-  sigma <- vcov(object, conf_bands = 0.5)[]
+  mean <- fitted(object, type = type)[]
+  sigma <- vcov(object, type = type)[]
 
   ll <- sum(vapply(seq_len(N), function(i) {
     dmvnorm(Y[i, ], mean[i, ], sigma, log = TRUE)
@@ -49,4 +49,46 @@ logLik.bvar <- function(object, ...) { # Todo: Maybe add conf_bands
   class(ll) <- "logLik"
 
   return(ll)
+}
+
+# To-do: WAIC
+#' @export
+WAIC <- function(x, ...) {UseMethod("WAIC", x)}
+
+
+#' @noRd
+WAIC.default <- function(x, ...) {
+  stop("No methods for class ", paste0(class(x), collapse = " / "), " found.")
+}
+
+
+#' @noRd
+WAIC.bvar <- function(x, n_thin = 1L, ...) {
+
+  Y <- x[["meta"]][["Y"]]
+  N <- x[["meta"]][["N"]]
+  K <- x[["meta"]][["K"]]
+
+  n_pres <- x[["meta"]][["n_save"]]
+  n_thin <- int_check(n_thin, min = 1, max = (n_pres / 10),
+    "Issue with n_thin. Maximum allowed is n_save / 10.")
+  n_save <- int_check((n_pres / n_thin), min = 1)
+
+  # Matrix of log-likelihoods
+  ll <- matrix(NA_real_, N, n_save)
+  for(s in seq_len(n_save)) {
+    mean <- x[["meta"]][["X"]] %*% x[["beta"]][s, , ]
+    sigma <- x[["sigma"]][s, , ]
+    ll[, s] <- vapply(seq_len(N), function(i) {
+      dmvnorm(Y[i, ], mean[i, ], sigma, log = TRUE)
+    }, numeric(1L))
+  }
+
+  # WAIC
+  lppds <- log(rowMeans(exp(ll))) # log pointwise predictive densities
+  res <- ll - matrix(rowMeans(ll), N, n_save)
+  pWAIC <- sum(rowMeans(res * res) * (N / (N - 1))) # Gelman (2014)
+  WAIC <- -2 * sum(lppd) + 2 * pWAIC
+
+  return(WAIC)
 }
