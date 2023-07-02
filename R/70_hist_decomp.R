14cfbb8b5a2d88f48ea918847359d344834950eb
Nikolas Kuschnig <nkuschni@wu.ac.at>
2023-07-02 14:11:25 +0200

    Test hist-decomp, improve RMSE and LPS

--- R/70_hist_decomp.R
+++ R/70_hist_decomp.R
@@ -1,11 +1,13 @@
 
 #' Historical decomposition
 #'
-#' Function to compute a historical variance decomposition.
+#' Function to compute a historical variance decomposition of a VAR.
 #'
-#' @param type
+#' @param x A \code{bvar} object, obtained from \code{\link{bvar}}.
+#' @param type Character scalar. Whether to use median or mean values.
 #'
-#' @return
+#' @return Returns a numerical array (time, variable, shock) with the results
+#' of the historical decomposition.
 #'
 #' @keywords BVAR historical decomposition hd
 #'
@@ -26,13 +28,20 @@
 hist_decomp.bvar <- function(x, type = c("mean", "median")) {
 
   type <- match.arg(type)
-  out <- compute_hd(x, type)
+
+  # Necessary quantities
+  shock <- matrix(0, x[["meta"]][["K"]] - 1, x[["meta"]][["M"]])
+  shock[seq(x[["meta"]][["M"]]), ] <- t(chol(vcov(x, type = type)))
+  comp <- companion(x, type = type)
+  eps <- solve(shock[seq(x[["meta"]][["M"]]), ], t(residuals(x, type = type)))
+
+  out <- compute_hd(x, shock, comp, eps)
 
   return(out)
 }
 
 
-#' @rdname irf.bvar
+#' @rdname hist_decomp.bvar
 #' @export
 hist_decomp <- function(x, ...) {UseMethod("hist_decomp", x)}
 
@@ -43,18 +52,27 @@ hist_decomp.default <- function(x, ...) {
 }
 
 
+#' Historical variance decompostions draws
+#'
+#' Computes forecast error variance decompostions (FEVDs) using the impulse
+#' response draws obtained from \code{\link{compute_irf}}.
+#'
+#' @param x Numeric matrix. Contains a draw of impulse responses
+#' obtained from \code{\link{compute_irf}}.
+#' @param shock Numeric matrix with the shocks.
+#' @param comp Numeric matrix with the coefficients in companion form.
+#' @param eps Numeric matrix of the inverse shock times residuals.
+#' @param lags,N,M,K Integer scalar. Dimensions of the VAR.
+#'
+#' @return Returns a numeric array of FEVDs.
+#'
 #' @noRd
-compute_hd <- function(x, type,
+compute_hd <- function(x, shock, comp, eps,
   lags = x[["meta"]][["lags"]], N = x[["meta"]][["N"]],
   M = x[["meta"]][["M"]], K = x[["meta"]][["K"]]) {
 
-  shock <- matrix(0, K - 1, M)
-  shock[seq(M), ] <- t(chol(vcov(x, type = type)))
-  comp <- companion(x, type = type)
-  eps <- solve(shock, t(residuals(x, type = type)))
-
   kind_of_I <- cbind(diag(M), matrix(0, M, (lags - 1) * M))
-  hd_comp <- array(0, dim = c(M, N + 1, M))
+  hd_decomp <- array(0, dim = c(M, N + 1, M))
   tmp <- array(0, dim = c(K - 1, N + 1, M))
 
   for(j in seq_len(dim(hd_decomp)[3])) { # Over variables
@@ -63,9 +81,9 @@ compute_hd <- function(x, type,
 
     for(i in seq.int(2, dim(hd_decomp)[2])) { # Over observations
       tmp[, i, j] <- shock %*% eps_tmp[, i] + comp %*% tmp[, i - 1, j]
-      hd_comp[, i, j] <- kind_of_I %*% tmp[, i, j]
+      hd_decomp[, i, j] <- kind_of_I %*% tmp[, i, j]
     }
   }
 
-  return(aperm(hd_comp, c(2, 1, 3)))
+  return(aperm(hd_decomp, c(2, 3, 1)))
 }
--- R/71_rmse.R
+++ R/71_rmse.R
@@ -1,17 +1,94 @@
 
-rmse <- \(x) {
-  apply(resid(x), 2, \(r) sqrt(sum(r^2) / length(r)))
+#' Model fit in- and out-of-sample
+#'
+#' Functions to compute the root mean squared error and log predictive scores.
+#'
+#' @param x A \code{bvar} object, obtained from \code{\link{bvar}}.
+#' @param holdout Optional numeric matrix or dataframe. Used for the
+#' out-of-sample fit.
+#'
+#' @return Returns a matrix with measures of model fit.
+#'
+#' @keywords BVAR RMSE LPS
+#'
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' # Access a subset of the fred_qd dataset
+#' data <- fred_qd[, c("CPIAUCSL", "UNRATE", "FEDFUNDS")]
+#' # Transform it to be stationary
+#' data <- fred_transform(data, codes = c(5, 5, 1), lag = 4)
+#'
+#' # Estimate a BVAR using one lag, default settings and very few draws
+#' x <- bvar(data[seq(1, nrow(data) - 5)], lags = 1,
+#'   n_draw = 600L, n_burn = 100L, verbose = FALSE)
+#'
+#' # Compute RMSE
+#' rmse(x)
+#' lps(x, holdout = data[seq(nrow(data) - 4, nrow(data)), ])
+rmse.bvar <- function(x, holdout) {
+
+  if(missing(holdout)) { # In-sample
+    apply(resid(x, type = "mean"), 2, function(r) sqrt(sum(r^2) / length(r)))
+  } else { # Out-of-sample
+    fit <- apply(predict(x, horizon = NROW(holdout))$fcast, c(2, 3), mean)
+    err <- fit - holdout
+    apply(err, 2, function(r) sqrt(sum(r^2) / length(r)))
+  }
 }
 
-lps <- \(x) {
-  fit <- array(NA, dim = c(1000, x$meta$N, x$meta$M))
-  for(i in seq(1000)) {
-    fit[i, , ] <- x$meta$X %*% x$beta[i, , ]
+#' @rdname rmse.bvar
+#' @export
+lps.bvar <- function(x, holdout, n_thin = 1L) {
+
+  n_pres <- x[["meta"]][["n_save"]]
+  n_thin <- int_check(n_thin, min = 1, max = (n_pres / 10),
+    "Issue with n_thin. Maximum allowed is (n_draw - n_burn) / 10.")
+  n_save <- int_check((n_pres / n_thin), min = 1)
+
+  if(missing(holdout)) { # In-sample
+    # We need the mean and sd from these fits # To-do: adapt `resid`
+    fit <- array(NA, dim = c(n_save, x[["meta"]][["N"]], x[["meta"]][["M"]]))
+    i <- 1
+    for(s in sample(n_pres, n_save)) {
+      fit[i, , ] <- x[["meta"]][["X"]] %*% x[["beta"]][s, , ]
+      i <- i + 1L
+    }
+    Y <- x[["meta"]][["Y"]]
+  } else { # Out-of-sample
+    fit <- predict(x, horizon = NROW(holdout))$fcast
+    Y <- holdout
   }
   mu <- apply(fit, c(2, 3), mean)
   sd <- apply(fit, c(2, 3), sd)
-  lps <- matrix(NA, x$meta$N, x$meta$M)
-  for(j in seq(x$meta$M)) {
-    lps[, j] <- dnorm(x$meta$Y[, j] - mu[, j], sd = sd[, j], log = TRUE)
+  lps <- matrix(NA, NROW(mu), NCOL(mu))
+  for(j in seq_len(ncol(lps))) {
+    lps[, j] <- dnorm(Y[, j] - mu[, j], sd = sd[, j], log = TRUE)
   }
+
+  return(lps)
+}
+
+
+
+#' @rdname rmse.bvar
+#' @export
+rmse <- function(x, ...) {UseMethod("rmse", x)}
+
+
+#' @noRd
+rmse.default <- function(x, ...) {
+  stop("No methods for class ", paste0(class(x), collapse = " / "), " found.")
+}
+
+
+#' @rdname rmse.bvar
+#' @export
+lps <- function(x, ...) {UseMethod("lps", x)}
+
+
+#' @noRd
+lps.default <- function(x, ...) {
+  stop("No methods for class ", paste0(class(x), collapse = " / "), " found.")
 }
